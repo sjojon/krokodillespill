4d0146f398a456b6540e1159b16c0f9926818a9b
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-04 15:19:43 +0200

    Initial commit

---
+++ krokodillespill/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace krokodillespill
+{
+    internal class Program
+    {
+        //private static Random random = new Random();
+
+
+        static void Main(string[] args)
+        {
+            int points = 0;
+            bool flag = true;
+            while (flag)
+            {
+                var random = new Random();
+                var tall1 = random.Next(0, 12);
+                var tall2 = random.Next(0, 12);
+
+                Console.WriteLine("Poeng: " + points);
+                Console.WriteLine("Use: <, > or =");
+                Console.WriteLine(tall1 + " _ " + tall2);
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "<":
+                    case ">":
+                    case "=":
+                    {
+                        if ((tall1 > tall2 && input == ">") || (tall1 < tall2 && input == "<") ||
+                            (tall1 == tall2 && input == "="))
+                        {
+                            points++;
+                        }
+                        else
+                        {
+                            points--;
+                        }
+
+                        Console.Clear();
+                        break;
+                    }
+                    default:
+                        Console.WriteLine("Ikke gyldig tegn!");
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
